e6c61665412e2aaf2a59569d89ac259e57cf6a2c
erikuzhuo <erikuzhuo01@gmail.com>
2024-10-24 22:30:40 +0800

    nop bai

--- src/src/ContactsManagement/Controller/ContactsController.java
+++ src/src/ContactsManagement/Controller/ContactsController.java
@@ -24,17 +24,17 @@ public class ContactsController {
 
 
         do {
-            System.out.println("How many new contacts? ");
+            System.out.println("Thêm mới bao nhiêu số điện thoại? ");
             int newAddNumber = Integer.parseInt(scanner.nextLine());
             do {
                 try {
                     if (newAddNumber < 0) {
-                        System.out.println("Please re-input number of new contacts: ");
+                        System.out.println("Mời nhập lại số lượng: ");
                         newAddNumber = Integer.parseInt(scanner.nextLine());
                         check = false;
                     } else check = true;
                 } catch (NumberFormatException e) {
-                    System.out.println("Number must input correctly.");
+                    System.out.println("Số lượng cần nhập đúng.");
                     check = false;
                 }
             } while (!check);
@@ -46,10 +46,10 @@ public class ContactsController {
 
                 // input phone number
                 do {
-                    System.out.println("Input 10 digits phone number of staff number " + (i + 1) + " starting by 0:");
+                    System.out.println("Xin mời nhập số đt của người thứ " + (i + 1) + " bắt đầu bằng số 0 với 10 số:");
                     tempNumber = scanner.nextLine();
                     if (!PatternFormat.checkPhoneNumber(tempNumber)) {
-                        System.out.println("Number format not correct. Kindly re-input.");
+                        System.out.println("Xin mời nhập đúng yêu cầu.");
                         check = false;
                     } else if (!listOfContact.isEmpty()) {
                         if (checkDataIsExist.checkNumberExist(listOfContact, tempNumber)
@@ -62,9 +62,10 @@ public class ContactsController {
 
                 // input name
                 do {
-                    System.out.println("Input name of contact " + (i + 1) + ":");
+                    System.out.println("mời nhập tên của người " + (i + 1) + ":");
                     tempName = scanner.nextLine();
                     if (Objects.equals(tempName, "") || !PatternFormat.namePattern(tempName)) {
+                        System.out.println("Xin mời nhập đúng tên.");
                         check = false;
                     } else check = true;
                 } while (!check);
@@ -72,33 +73,33 @@ public class ContactsController {
 
                 // input gender
                 do {
-                    System.out.println("Input gender of contact " + (i + 1) + ": (M/F)");
+                    System.out.println("Mời nhập giới tính của người " + (i + 1) + ": (M/F)");
                     tempGender = scanner.nextLine();
                     if (!tempGender.equals("M") && !tempGender.equals("F")) {
                         check = false;
-                        System.out.println("Invalid input. Kindly re-input.");
+                        System.out.println("Mời nhập đúng theo yêu cầu.");
                     } else check = true;
                 } while (!check);
                 contactPerson.setGender(tempGender);
 
                 // input BD
                 do {
-                    System.out.println("Input birth date of contact " + (i + 1) + ": (YYYY/MM/DD)");
+                    System.out.println("Mời nhập ngày sinh của người " + (i + 1) + ": (YYYY/MM/DD)");
                     tempBirthDate = scanner.nextLine();
                     try {
                         LocalDate parsedDate = LocalDate.parse(tempBirthDate, dateTimeFormatter);
 
                         if (parsedDate.isBefore(minBirthDate)) {
-                            System.out.println("Date is too early. Only after 1940.");
+                            System.out.println("Năm sinh sớm nhất là 1940 ");
                             check = false;
                         } else if (parsedDate.isAfter(today)) {
-                            System.out.println("Date cannot be later than today.");
+                            System.out.println("Không thể trể hơn hôm nay.");
                             check = false;
                         } else {
                             check = true;
                         }
                     } catch (DateTimeParseException e) {
-                        System.out.println("Date format not correct. Kindly re-input.");
+                        System.out.println("Mời nhập đúng mặc định.");
                         check = false;
                     }
                 } while (!check);
@@ -106,9 +107,10 @@ public class ContactsController {
 
                 // input address
                 do {
-                    System.out.println("Input address of contact " + (i + 1) + ":");
+                    System.out.println("mời  nhập địa chỉ người thứ " + (i + 1) + ":");
                     tempAdress = scanner.nextLine();
                     if (Objects.equals(tempAdress, "")) {
+                        System.out.println("Xin mời nhập đúng địa chỉ.");
                         check = false;
                     } else check = true;
                 } while (!check);
@@ -116,9 +118,10 @@ public class ContactsController {
 
                 // input group
                 do {
-                    System.out.println("Input group of contact " + (i + 1) + ":");
+                    System.out.println("Nhập tên nhóm số đt " + (i + 1) + ":");
                     tempGroup = scanner.nextLine();
                     if (Objects.equals(tempGroup, "")) {
+                        System.out.println("Xin mời nhập đúng tên nhóm.");
                         check = false;
                     } else check = true;
                 } while (!check);
@@ -126,10 +129,10 @@ public class ContactsController {
 
                 // input email
                 do {
-                    System.out.println("Input email of contact " + (i + 1) + ":");
+                    System.out.println("Mời nhập địa chỉ email " + (i + 1) + ":");
                     tempEmail = scanner.nextLine();
                     if (!PatternFormat.checkEmail(tempEmail)) {
-                        System.out.println("Email format not correct. Kindly re-input.");
+                        System.out.println("Email không đúng, mời nhập lại..");
                         check = false;
                     } else check = true;
                 } while (!check);
@@ -138,17 +141,18 @@ public class ContactsController {
                 tempPersonList[i] = contactPerson;
             }
             // confirm
-            System.out.println("Did you confirm to add on below contacts list: (Y/N) \n" +
+            System.out.println("Bạn có chắc lưu danh sách dưới đây vào bộ nhớ: (Y/N) \n" +
                     Arrays.toString(tempPersonList));
             confirm = scanner.nextLine();
 
         } while (!confirm.equalsIgnoreCase("Y"));
 
         listOfContact.addAll(Arrays.asList(tempPersonList));
-        System.out.println("Add on Successfully.");
+        System.out.println("Lưu thành công.");
     }
 
     public static void displayListOfContact(List<Person> listOfContact) {
+        System.out.println("Danh sách danh bạ trong bộ nhớ: \n");
         for (Person staff : listOfContact) {
             System.out.println(staff.toString());
         }
@@ -160,10 +164,10 @@ public class ContactsController {
         boolean check = true;
         do {
             do {
-                System.out.println("Input delete number: ");
+                System.out.println("Nhập số đt cần xóa: ");
                 tempNumber = scanner.nextLine();
                 while (tempNumber.isEmpty() || !checkDataIsExist.checkNumberExist(listOfContact, tempNumber)) {
-                    System.out.println("Invalid number. Please re-input. | Or input 0 to exit.");
+                    System.out.println("Không đúng mời nhập lại | hoặc nhập 0 để thoát.");
                     tempNumber = scanner.nextLine();
                     if (tempNumber.equals("0")) break;
                 }
@@ -175,18 +179,21 @@ public class ContactsController {
                         break;
                     }
                 }
-                System.out.println("Are you sure want to delete this staff: (Y/N) \n"
+                System.out.println("Bạn có chắc xóa danh bạ này? (Y/N) \n"
                         + (deleteContact != null ? deleteContact.toString() : null));
                 confirm = scanner.nextLine();
 
             } while (!confirm.equalsIgnoreCase("Y"));
             if (tempNumber.equals("0")) {
-                System.out.println("Exit and cancelled delete.");
+                System.out.println("Thoát.");
             } else {
                 listOfContact.remove(deleteContact);
-                System.out.println("Staff deleted successfully.");
+                System.out.println("Đã xóa. Danh bạ hiện tại. \n");
+                for (Person staff : listOfContact) {
+                    System.out.println(staff.toString());
+                }
             }
-            System.out.println("Do you still want to delete other contactf: (Y/N)");
+            System.out.println("Bạn có muốn xóa thêm? (Y/N)");
             confirm = scanner.nextLine();
         } while (!confirm.equalsIgnoreCase("N"));
     }
@@ -202,10 +209,10 @@ public class ContactsController {
 
         do {
             if (!listOfContact.isEmpty()) {
-                System.out.println("Input contact phone number that need update: ");
+                System.out.println("Nhập số đt cần cập nhật: ");
                 checkNumber = scanner.nextLine();
                 while (checkNumber.isEmpty() || !checkDataIsExist.checkNumberExist(listOfContact, checkNumber)) {
-                    System.out.println("Invalid number. Please re-input. | Or input 0 to exit.");
+                    System.out.println("Không đúng mời nhập lại | hoặc nhập 0 để thoát.");
                     checkNumber = scanner.nextLine();
                     if (Objects.equals(checkNumber, "0")) break;
                 }
@@ -220,23 +227,24 @@ public class ContactsController {
                 }
 
                 do {
-                    System.out.println("What's information number you want to update: \n" +
-                            "1. Name.\n" +
-                            "2. Birthday.\n" +
-                            "3. Gender.\n" +
-                            "4. address.\n" +
-                            "5. Group of phone number. \n" +
+                    System.out.println("Bạn muốn cập nhật gì: \n" +
+                            "1. Tên.\n" +
+                            "2. Ngày Sinh.\n" +
+                            "3. Giới Tính.\n" +
+                            "4. Đại Chỉ.\n" +
+                            "5. Nhóm Đt. \n" +
                             "6. Email.\n " +
-                            "7. exit.");
+                            "7. Thoát.");
 
                     option = Integer.parseInt(scanner.nextLine());
                     switch (option) {
                         case 1:
                             do {
-                                System.out.println("Input new name of " + updateContact.getPhoneNumber());
+                                System.out.println("Nhập tên mới của " + updateContact.getPhoneNumber());
                                 updateContact.setName(scanner.nextLine());
                                 tempName = scanner.nextLine();
                                 if (Objects.equals(tempName, "") || !PatternFormat.namePattern(tempName)) {
+                                    System.out.println("Mời nhập đúng tên.");
                                     check = false;
                                 } else check = true;
                             } while (!check);
@@ -244,22 +252,22 @@ public class ContactsController {
                             break;
                         case 2:
                             do {
-                                System.out.println("Input new BD of " + updateContact.getPhoneNumber());
+                                System.out.println("Ngày sinh mới của " + updateContact.getPhoneNumber());
                                 tempBirthDate = scanner.nextLine();
                                 try {
                                     LocalDate parsedDate = LocalDate.parse(tempBirthDate, dateTimeFormatter);
 
                                     if (parsedDate.isBefore(minBirthDate)) {
-                                        System.out.println("Date is too early. Only after 1940.");
+                                        System.out.println("Không sớm hơn 1940.");
                                         check = false;
                                     } else if (parsedDate.isAfter(today)) {
-                                        System.out.println("Date cannot be later than today.");
+                                        System.out.println("Không trễ hơn hôm nay.");
                                         check = false;
                                     } else {
                                         check = true;
                                     }
                                 } catch (DateTimeParseException e) {
-                                    System.out.println("Date format not correct. Kindly re-input.");
+                                    System.out.println("Mời nhập theo mặc định.");
                                     check = false;
                                 }
                             } while (!check);
@@ -267,20 +275,21 @@ public class ContactsController {
                             break;
                         case 3:
                             do {
-                                System.out.println("Input gender of " + updateContact.getPhoneNumber() + ": (M/F)");
+                                System.out.println("Giới tính mới của " + updateContact.getPhoneNumber() + ": (M/F)");
                                 tempGender = scanner.nextLine();
                                 if (!tempGender.equals("M") && !tempGender.equals("F")) {
                                     check = false;
-                                    System.out.println("Invalid input. Kindly re-input.");
+                                    System.out.println("Không đúng mời nhập lại.");
                                 } else check = true;
                             } while (!check);
                             updateContact.setGender(tempGender);
                             break;
                         case 4:
                             do {
-                                System.out.println("Input new address of " + updateContact.getPhoneNumber());
+                                System.out.println("Địa chỉ mới của " + updateContact.getPhoneNumber());
                                 tempAdress = scanner.nextLine();
                                 if (Objects.equals(tempAdress, "")) {
+                                    System.out.println("Mời nhập đúng địa chỉ.");
                                     check = false;
                                 } else check = true;
                             } while (!check);
@@ -288,9 +297,10 @@ public class ContactsController {
                             break;
                         case 5:
                             do {
-                                System.out.println("Input new position of " + updateContact.getPhoneNumber());
+                                System.out.println("Mời nhập nhóm ĐT mới của " + updateContact.getPhoneNumber());
                                 tempGroup = scanner.nextLine();
                                 if (Objects.equals(tempGroup, "")) {
+                                    System.out.println("Mời nhập đúng tên nhóm.");
                                     check = false;
                                 } else check = true;
                             } while (!check);
@@ -298,10 +308,10 @@ public class ContactsController {
                             break;
                         case 6:
                             do {
-                                System.out.println("Input new email of " + updateContact.getPhoneNumber());
+                                System.out.println("Email mới của " + updateContact.getPhoneNumber());
                                 tempEmail = scanner.nextLine();
                                 if (!PatternFormat.checkEmail(tempEmail)) {
-                                    System.out.println("Email format not correct. Kindly re-input.");
+                                    System.out.println("Email không đúng mời nhập lại.");
                                     check = false;
                                 } else check = true;
                             } while (!check);
@@ -314,7 +324,7 @@ public class ContactsController {
                     }
                     if (!continueEditing) break;
                     else {
-                        System.out.println("New update as below. Do you still want to update other information? (Y/N)\n"
+                        System.out.println("Dữ liệu cập nhật mới như sau, bạn cần điều chỉnh khác? (Y/N)\n"
                                 + updateContact.toString());
                         confirm = scanner.nextLine();
                     }
@@ -322,20 +332,20 @@ public class ContactsController {
 
                 if (!continueEditing) break;
                 else {
-                    System.out.println("Update completed. Do you want update other contact? (Y/N)");
+                    System.out.println("Cập nhật thành công. Bạn cần điều chỉnh danh bạ khác? (Y/N)");
                     confirm = scanner.nextLine();
                 }
             } else {
-                System.out.println("List is empty.");
+                System.out.println("Bộ nhớ trống.");
                 continueEditing = false;
                 break;
             }
         } while (!confirm.equalsIgnoreCase("N"));
         if (!continueEditing) {
-            System.out.println("Cancelled Editing.");
+            System.out.println("Hủy hiệu chỉnh.");
         } else {
             listOfContact.set(index, updateContact);
-            System.out.println("Update completed.");
+            System.out.println("Cập nhật thành công.");
         }
     }
 
@@ -346,7 +356,7 @@ public class ContactsController {
 
         do {
             do {
-                System.out.println("Input contact phone number that need to find: ");
+                System.out.println("Số Đt hặc tên cần tìm: ");
                 checkNumber = scanner.nextLine();
                 while (checkNumber.isEmpty() || !checkDataIsExist.checkNumberExist(listOfContact, checkNumber)) {
                     System.out.println("Invalid number. Please re-input. | Or input 0 to exit.");
